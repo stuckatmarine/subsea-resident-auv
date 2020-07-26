f49a52549cefcc3467d94d88f2105238767dd44f
stuckatmarine <mwd467@mun.ca>
2020-07-26 11:56:09 -0230

    SRAUV now pancake shape, scene updates

---
+++ Assets/Scripts/CameraSaver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSaver : MonoBehaviour
+{
+    private bool saving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (saving)
+            {
+                stopSaving();
+            }
+            else
+                startSaving();
+
+            Debug.Log("Saving: " + saving);
+        }
+    }
+
+    private void stopSaving()
+    {
+        saving = false;
+    }
+
+    private void startSaving()
+    {
+        saving  = true;
+    }
+}
