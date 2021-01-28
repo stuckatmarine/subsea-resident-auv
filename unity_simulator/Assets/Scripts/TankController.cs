f56e4284638fbf22e1b1d79efa7f69d38ffcbd55
Andrew Nash <33398082+andrewnash@users.noreply.github.com>
2021-01-27 22:22:01 -0330

    fix tank hitbox

---
+++ unity_simulator/Assets/Scripts/TankController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+
+public class TankController : MonoBehaviour
+{
+    public void Awake()
+    {
+        Academy.Instance.OnEnvironmentReset += EnvironmentReset;
+    }
+
+    void EnvironmentReset()
+    {
+        Debug.Log("resetting!!");
+    }
+}
