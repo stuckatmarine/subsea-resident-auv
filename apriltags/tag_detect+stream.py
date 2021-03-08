33dab2641a5076a1895ff65d692f074fc4efccf5
Mark Belbin <msbelbin@mun.ca>
2021-03-08 00:22:29 -0330

    Added Tank Localization
    - Calculated homogeneous transform expression to map camera to tag pose to AUV to tank position.
    - Also added video recording using opencv, going to have to try this on the Pi !

--- apriltags/tag_detect+stream.py
+++ apriltags/tag_detect+stream.py
@@ -1,3 +1,11 @@
+######################################
+# APRIL Tags & Camera Pose
+#
+# Completed for 2021 SRAUV Capstone
+# and ECE 8410 Term Project
+#
+# Mark Belbin - 2021
+######################################
 import numpy as np
 import cv2
 import time
@@ -14,13 +22,56 @@ at_detector = Detector(families='tag36h11',
                        debug=0)
 
 # Global Camera Pose Parameters
-gTag_pose_t = np.array([0,0,0])
-gTag_pose_R = np.array([[0,0,0],[0,0,0],[0,0,0]])
-gTag_pose_T = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
-
+gCam_pose_t = np.array([0,0,0])
+gCam_pose_R = np.array([[0,0,0],[0,0,0],[0,0,0]])
+gCam_pose_T = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
 gRx = 0.0
 gRy = 0.0
 gTID = None
+
+# Transformation Matrices #
+
+# Tank to Marker Transforms
+gTank_T_Marker1 = np.array([[0.0, 0.0,-1.0, 0.0],
+                            [1.0, 0.0, 0.0, 2.0],
+                            [0.0,-1.0, 0.0, 2.0],
+                            [0.0, 0.0, 0.0, 1.0]])
+
+gTank_T_Marker2 = np.array([[1.0, 0.0, 0.0, 2.0],
+                            [0.0, 0.0, 1.0, 4.0],
+                            [0.0,-1.0, 0.0, 2.0],
+                            [0.0, 0.0, 0.0, 1.0]])
+
+gTank_T_Marker3 = np.array([[ 0.0, 0.0, 1.0, 4.0],
+                            [-1.0, 0.0, 0.0, 2.0],
+                            [ 0.0,-1.0, 0.0, 2.0],
+                            [ 0.0, 0.0, 0.0, 1.0]])
+
+gTank_T_Marker4 = np.array([[-1.0, 0.0, 0.0, 2.0],
+                            [ 0.0, 0.0,-1.0, 0.0],
+                            [ 0.0,-1.0, 0.0, 2.0],
+                            [ 0.0, 0.0, 0.0, 1.0]])
+
+# Camera to AUV transforms
+gFrontCam_T_AUV = np.array([[1.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0,-1.0, 0.0],
+                            [0.0, 1.0, 0.0,-0.184],
+                            [0.0, 0.0, 0.0, 1.0]])
+
+gBackCam_T_AUV = np.array([[-1.0, 0.0, 0.0, 0.0  ],
+                            [ 0.0, 0.0,-1.0, 0.0  ],
+                            [ 0.0,-1.0, 0.0,-0.184],
+                            [ 0.0, 0.0, 0.0, 1.0  ]])
+
+# Global AUV Parameters
+gAUVx = 0.0
+gAUVy = 0.0
+gAUVz = 0.0
+gAUVheading = 0.0
+
+# Output video parameters
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video_out = cv2.VideoWriter('Video/out.avi',fourcc, 20, (640,480))
 
 print("Camera sink open, Waiting for camera feed...")
 
@@ -66,51 +117,65 @@ while True:
         (cX, cY) = (int(tag.center[0]), int(tag.center[1]))
         cv2.circle(frame, (cX, cY), 5, (0, 0, 255), -1)
         
-        #print(tag.pose_R)
-        gTag_pose_t = tag.pose_t
-        gTag_pose_R = tag.pose_R
-        #gTag_pose_H = np.array([[gTag_pose_R[0,0],gTag_pose_R[0,1],gTag_pose_R[0,2],gTag_pose_t[0,0]],
-        #                        [gTag_pose_R[1,0],gTag_pose_R[1,1],gTag_pose_R[1,2],gTag_pose_t[1,0]],
-        #                        [gTag_pose_R[2,0],gTag_pose_R[2,1],gTag_pose_R[2,2],gTag_pose_t[2,0]],
-        #                        [0.0,0.0,0.0,1.0]])
+        # Calculate camera pose parameters
+        gCam_pose_t = tag.pose_t
+        gCam_pose_R = tag.pose_R
+        gCam_pose_T = np.vstack((np.hstack((gCam_pose_R, gCam_pose_t)), np.array([0.0,0.0,0.0,1.0])))
 
-        gTag_pose_T = np.vstack((np.hstack((gTag_pose_R, gTag_pose_t)), np.array([0.0,0.0,0.0,1.0])))
-
-        gRx = math.atan2(gTag_pose_R[2,1], gTag_pose_R[1,1]) * 180.0 / math.pi
-        gRy = math.atan2(gTag_pose_R[0,2], gTag_pose_R[0,0]) * 180.0 / math.pi
-
+        gRx = math.atan2(gCam_pose_R[2,1], gCam_pose_R[1,1]) * 180.0 / math.pi
+        gRy = math.atan2(gCam_pose_R[0,2], gCam_pose_R[0,0]) * 180.0 / math.pi
         gTID = tag.tag_id
 
-        print(gTag_pose_T)
-        print("Camera X Rotation: " + str(gRx))
-        print("Camera Y Rotation: "+ str(gRy))
+        # Calculate AUV frame relative to tank frame
+        if (gCam_pose_T.any()):
+            if tag.tag_id == 0:
+                Tank_T_AUV = gTank_T_Marker1 @ np.linalg.inv(gCam_pose_T) @ gFrontCam_T_AUV
+            elif tag.tag_id == 1:
+                Tank_T_AUV = gTank_T_Marker2 @ np.linalg.inv(gCam_pose_T) @ gFrontCam_T_AUV
+            elif tag.tag_id == 2:
+                Tank_T_AUV = gTank_T_Marker3 @ np.linalg.inv(gCam_pose_T) @ gFrontCam_T_AUV
+            elif tag.tag_id == 3:
+                Tank_T_AUV = gTank_T_Marker4 @ np.linalg.inv(gCam_pose_T) @ gFrontCam_T_AUV
+        
+        # Calculte AUV parameters
+        gAUVx = Tank_T_AUV[0, 3]
+        gAUVy = Tank_T_AUV[1, 3]
+        gAUVz = Tank_T_AUV[2, 3]
+        gAUVheading = math.atan2(Tank_T_AUV[1,0], Tank_T_AUV[0,0]) * 180.0 / math.pi
 
     # Add Pose details to frame view if Tag detected
     if(gTID is not None):
-        cv2.putText(frame, "Z: " + f'{gTag_pose_t[2,0]:.3f}' + "m", (50,440), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
-        cv2.putText(frame, "Y: " + f'{gTag_pose_t[1,0]:.3f}' + "m", (50,420), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
-        cv2.putText(frame, "X: " + f'{gTag_pose_t[0,0]:.3f}' + "m", (50,400), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "CAM_X: " + f'{gCam_pose_t[0,0]:.3f}' + "m", (50,400), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "CAM_Y: " + f'{gCam_pose_t[1,0]:.3f}' + "m", (50,420), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "CAM_Z: " + f'{gCam_pose_t[2,0]:.3f}' + "m", (50,440), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
 
-        cv2.putText(frame, "ThetaX: " + f'{(gRx):.3f}' + " Deg", (200,400), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
-        cv2.putText(frame, "ThetaY: " + f'{(gRy):.3f}' + " Deg", (200,420), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "CAM_ThetaX: " + f'{(gRx):.3f}' + " Deg", (200,400), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "CAM_ThetaY: " + f'{(gRy):.3f}' + " Deg", (200,420), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
         cv2.putText(frame, "TagID: " + str(gTID), (200,440), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
-        
-        cv2.putText(frame, "Detect Time: " + f'{(time_detect*1000):.2f}' + " ms", (400,400), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "Detect Time: " + f'{(time_detect*1000):.2f}' + " ms", (200,460), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
 
+        cv2.putText(frame, "AUV_X: " + f'{gAUVx:.3f}' + "m", (440,400), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "AUV_Y: " + f'{gAUVy:.3f}' + "m", (440,420), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "AUV_Z: " + f'{gAUVz:.3f}' + "m", (440,440), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        cv2.putText(frame, "AUV_Yaw: " + f'{gAUVheading:.3f}' + " Deg", (440,460), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+    
+    # Write frame to video
+    video_out.write(frame)
 
     # Show the output frame after AprilTag detection
     cv2.imshow('receive', frame)
 
+    # Check for 'q' press in order to quit program
     if cv2.waitKey(1)&0xFF == ord('q'):
         break
 
+    # Check for 'c' press to capture a single frame
     if cv2.waitKey(1)&0xFF == ord('c'):
         cv2.imwrite("Frames/capture" + f'{cap_count:03d}' + ".jpg", frame)
         print("Frame " + f'{cap_count:03d}' + " Captured!")
         cap_count = cap_count + 1
 
-
-
+# Once finished, release / destroy windows
+video_out.release()
 cap_receive.release()
-
 cv2.destroyAllWindows()
