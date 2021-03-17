b24a10a20e89c58fae12550a3f13854d1d3b123c
Andrew Nash <33398082+andrewnash@users.noreply.github.com>
2021-03-17 19:23:44 -0230

    convert unity models and kinda use them

---
+++ MLTraining/convert_model.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+
+# first run onnx-tf convert -i "pilot.onnx" -o  "pilot.pb"
+path = 'results/realisticTankV1.15_hackVel_revertedThrust/pilot.pb'
+
+# TODO: look into tf.lite.Optimize
+converter = tf.lite.TFLiteConverter.from_saved_model(path) 
+tf_lite_model = converter.convert()
+open('pilot.tflite', 'wb').write(tf_lite_model)
